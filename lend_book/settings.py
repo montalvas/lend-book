638aa2f1b05f1dbc8273f1a14388730acc221f53
montalvas <lucas_montalvani@hotmail.com>
2022-01-13 23:26:15 -0300

    edit directory path

--- lend_book/settings.py
+++ lend_book/settings.py
@@ -11,11 +11,12 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+from pathlib import Path
 import django_heroku
 import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
